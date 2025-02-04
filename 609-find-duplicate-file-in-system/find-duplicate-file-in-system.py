e09f428337820600dc4769d2bb48cb5a533fe243
AlazarGebre <151013856+AlazarGebre21@users.noreply.github.com>
2025-02-04 17:36:38 +0300

    Time: 22 ms (42.29%) | Memory: 24.9 MB (77.99%) - LeetSync

--- 609-find-duplicate-file-in-system/find-duplicate-file-in-system.py
+++ 609-find-duplicate-file-in-system/find-duplicate-file-in-system.py
@@ -1,23 +1,24 @@
 class Solution:
     def findDuplicate(self, paths: List[str]) -> List[List[str]]:
-        path_dict ={}
+        path_dict = {}
         result = []
-        for path in paths:
-            lst = path.split()
-            index = 1
-            while index < len(lst):
-                files, content = lst[index][:-1].split("(")
-                abs_path = lst[0]+'/'+files
-                if content in path_dict:
-                    path_dict[content].append(abs_path)
+        for i in range(len(paths)):
+            path_array = paths[i].split()
+            root = path_array[0]
+            for j in range(1,len(path_array)):
+                index1 = path_array[j].index('(')
+                index2 = path_array[j].index(')')
+                key = path_array[j][index1 + 1:index2]
+                value = path_array[j][:index1]
+                if key in path_dict:
+                    path_dict[key].append(root + '/' + value)
                 else:
-                    path_dict[content] = [abs_path]
-                index += 1
-        for key, path in path_dict.items():
-            if len(path) == 1:
-                continue
-            else:
-                result.append(path)
+                    path_dict[key] = [root + '/' + value]
+        
+        for key, value in path_dict.items():
+            if len(value) > 1:
+                result.append(value)
 
         return result
 
+            
