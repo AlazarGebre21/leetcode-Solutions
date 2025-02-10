617a5f6e2e3693952d729182e164e75a0e0097b1
AlazarGebre <151013856+AlazarGebre21@users.noreply.github.com>
2025-02-10 11:32:50 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (17.69%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.py
+++ 861-flipping-an-image/flipping-an-image.py
@@ -1,14 +1,8 @@
 class Solution:
     def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
-        n = len(image)
-        for row in range(n):
-            for col in range(n):
-                if col < n - col - 1:
-                    image[row][col], image[row][n-col-1] = image[row][n-col-1], image[row][col]
-        for row in range(n):
-            for col in range(n):
-                if image[row][col] == 0:
-                    image[row][col] = 1
-                else:
-                    image[row][col] = 0
-        return image
+        result = []
+        for i in range(len(image)):
+            flip = image[i][::-1]
+            invert = list(map(lambda x: 1 - x, flip))
+            result.append(invert)
+        return result
