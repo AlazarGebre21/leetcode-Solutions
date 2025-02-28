2609dcf6a1a8e3888db792f951b6ac4da060ee00
AlazarGebre <151013856+AlazarGebre21@users.noreply.github.com>
2025-02-28 19:43:41 +0300

    Time: 28 ms (67.68%) | Memory: 21 MB (92.42%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
@@ -1,18 +1,11 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
         # creating a dictionary to store the remainder
-        my_dict = {0:1}
-        count = 0
-        sums = 0
-        modulo = 0
-
-        # Iterating through the array 
+        d = {0:1}
+        ans = sums = 0
         for num in nums:
             sums += num
-            modulo = sums % k
-            if modulo in my_dict:
-                count += my_dict[modulo]
-                my_dict[modulo] += 1
-            else:
-                my_dict[modulo] = my_dict.get(modulo,0) + 1
-        return count
+            mo = sums % k
+            ans += d.get(mo,0)
+            d[mo] = d.get(mo,0) + 1
+        return ans
