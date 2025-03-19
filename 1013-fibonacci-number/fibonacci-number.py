36f3cfad45b75f90d7f724169822815153c0cb56
AlazarGebre <151013856+AlazarGebre21@users.noreply.github.com>
2025-03-19 14:08:48 +0300

    Time: 39 ms (64.17%) | Memory: 17.8 MB (24.49%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,10 +1,25 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
-        
-        return self.fib(n-1) + self.fib(n-2)
+        my_dict = {}
+        def rec(n):
+            if n == 0:
+                return 0
+            elif n == 1:
+                return 1
+            
+            one = my_dict[n-1] if n-1 in my_dict else rec(n-1) 
 
-   
+            two = my_dict[n-2] if n-2 in my_dict else rec(n-2)
+
+            result = one + two
+
+            my_dict[n] = result
+            
+
+            return result
+        
+        return rec(n)
+
+
+        
+
