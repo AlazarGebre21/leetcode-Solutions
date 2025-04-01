cd9e001d6a851914f7d46a36121174ed4dd8d636
AlazarGebre <151013856+AlazarGebre21@users.noreply.github.com>
2025-04-02 00:55:12 +0300

    Time: 3 ms (18.12%) | Memory: 23 MB (37.34%) - LeetSync

--- 275-h-index-ii/h-index-ii.py
+++ 275-h-index-ii/h-index-ii.py
@@ -1,15 +1,28 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
 
+        def my_bisect_left(pos_val_to_be_inserted):
+            low = -1
+            high = len(citations)
+
+            while low + 1 < high:
+                mid = (low + high) // 2
+
+                if citations[mid] >= pos_val_to_be_inserted:
+                    high = mid
+                else:
+                    low = mid
+            
+            return high
+
         
         def is_it_h_index(h_index):
 
-            count = 0
-
-            h_papers = len(citations) - bisect_left(citations, h_index)
+            h_papers = len(citations) - my_bisect_left(h_index)
 
             if h_papers >= h_index:
                 return True
+
             return False
 
         low = 0
