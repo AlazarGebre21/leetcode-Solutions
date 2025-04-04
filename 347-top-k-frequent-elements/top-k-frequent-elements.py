b30a2121d021b9c1f5160dea7207a17260b7cc77
AlazarGebre <151013856+AlazarGebre21@users.noreply.github.com>
2025-04-04 22:21:05 +0300

    Time: 15 ms (10.35%) | Memory: 24.2 MB (13.25%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,16 +1,36 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        '''
+        I can simply count in dictionary and sort it but that would be nlogn time complexity so to make it o(N) i will use a bucket sort
+
+
+        This bucket array of array will hold the frequncy of each element for example for the array [8,8,8,5,2,4,5] the items will be put as such [[], [2, 4], [5], [8], [], [], [], []] so each index represents the frequency of the elements in the bucket
+        '''
+
         count = {}
-        freq = [[] for i in range(len(nums) + 1)]
+        bucket = [[] for i in range(len(nums) + 1)]
+        top_k_frequent = []
+        
 
         for num in nums:
-            count[num] = 1 + count.get(num, 0)
-        for num, cnt in count.items():
-            freq[cnt].append(num)
+            count[num] = count.get(num,0) + 1
         
-        res = []
-        for i in range(len(freq) - 1, 0, -1):
-            for num in freq[i]:
-                res.append(num)
-                if len(res) == k:
-                    return res
+        for val, freq in count.items():
+            bucket[freq].append(val)
+        
+        for i in range(len(bucket) - 1, -1, -1):
+            for num in bucket[i]:
+                if len(top_k_frequent) != k:
+                    top_k_frequent.append(num)
+                else:
+                    break
+        
+        
+        return top_k_frequent
+            
+        
+
+        
+
+
+        
