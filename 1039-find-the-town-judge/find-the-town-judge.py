544816541dcad9c03a4560ed1ff0c3264fdbaec4
AlazarGebre <151013856+AlazarGebre21@users.noreply.github.com>
2025-04-07 23:47:42 +0300

    Time: 28 ms (13.70%) | Memory: 21.2 MB (7.82%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        directed_graph = {}
+        judge = set()
+        is_it_judge = None
+
+        if not trust and n == 1:
+            return 1
+        elif not trust and n > 1:
+            return -1
+
+        for i in range(1,n+1):
+            directed_graph[i] = set()
+        
+        for _from, _to in trust:
+            directed_graph[_from].add(_to)
+        
+        print(directed_graph)
+
+        for key,values in directed_graph.items():
+            if not len(values):
+                is_it_judge = key
+            
+
+        if not is_it_judge:
+            return -1
+        
+        for key, value in directed_graph.items():
+            if key != is_it_judge and is_it_judge not in value:
+                return -1
+        
+        return is_it_judge
+
