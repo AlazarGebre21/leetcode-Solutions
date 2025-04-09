cadb8436799c915c7d3fd5d434e115efd127cc7a
AlazarGebre <151013856+AlazarGebre21@users.noreply.github.com>
2025-04-09 13:15:43 +0300

    Time: 35 ms (74.07%) | Memory: 18.1 MB (96.57%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -1,28 +1,31 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         
-        direction = [[1,0],[-1,0],[0,1],[0,-1]]
-
-        def in_bound(row,col):
-            return 0 <= row < len(grid) and 0 <= col < len(grid[0])
-
-
-        def neighbour_1(row,col):
-            count = 0
-            for i, j in direction:
-                new_row = row + i
-                new_col = col + j
-                if in_bound(new_row,new_col) and grid[new_row][new_col] == 1:
-                    count += 1
-            
-            return count
-
+        rows = len(grid)
+        cols = len(grid[0])
         perimeter = 0
-        for i in range(len(grid)):
-            negihbour_1 = 0
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    perimeter += (4 - neighbour_1(i,j))
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    max_perimeter = 4
+                    # checking top
+                    if r > 0 and grid[r-1][c] == 1:
+                        max_perimeter -= 1
+                    # checking bottom
+                    if r < rows - 1 and grid[r+1][c] == 1:
+                        max_perimeter -= 1
+                    # checking left
+                    if c > 0 and grid[r][c-1] == 1:
+                        max_perimeter -= 1
+                    # checking right
+                    if c < cols - 1 and grid[r][c+1] == 1:
+                        max_perimeter -= 1
+                    
+                    perimeter += max_perimeter
         
         return perimeter
+
+
+
                     
