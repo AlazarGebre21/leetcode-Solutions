738b7cb8a59614f703bfcb2f5a80128f5fe067c5
AlazarGebre <151013856+AlazarGebre21@users.noreply.github.com>
2025-04-19 16:07:53 +0300

    Time: 643 ms (5.03%) | Memory: 18.3 MB (74.21%) - LeetSync

---
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -0,0 +1,51 @@
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+
+        n = len(grid)
+        clear_path = False
+        que = deque([(0,0)])
+        count = 0
+
+        directions = [(1,0),(-1,0),(0,1),(0,-1),(-1,-1),(1,1),(-1,1),(1,-1)]
+
+        def inbound(r,c):
+            return 0<= r < n and 0 <= c < n
+        
+        if grid[0][0] == 1:
+            return -1
+        
+        
+        while que:
+
+            for _ in range(len(que)):
+
+                r, c = que.popleft()
+                grid[r][c] = 1
+
+                if r == n - 1 and c == n - 1:
+                    clear_path = True
+                    break
+
+                for dr, dc in directions:
+
+                    nr = r + dr
+                    nc = c + dc
+
+
+                    if inbound(nr,nc) and grid[nr][nc] == 0 and ((nr,nc)) not in que:
+                        que.append((nr,nc))
+                    
+            if not clear_path:
+                count += 1  
+            else:
+                break
+                
+        
+
+        return count + 1 if clear_path else -1
+
+        
+
+
+
+
