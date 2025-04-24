f111029ed40277e5b10fc06d0e65fa64c297eda8
AlazarGebre <151013856+AlazarGebre21@users.noreply.github.com>
2025-04-25 00:36:36 +0300

    Time: 4 ms (68.67%) | Memory: 19.5 MB (22.17%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,7 +1,7 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = defaultdict(list)
-        color = [1] * numCourses
+        c = [1] * numCourses
 
         for _ከዛ, _መጀመራያ in  prerequisites:
             graph[_መጀመራያ].append(_ከዛ)
@@ -9,21 +9,21 @@ class Solution:
 
         def cycle(vertex):
 
-            if color[vertex] == 2:
+            if c[vertex] == 2:
                 return True
-            if color[vertex] == 3:
+            if c[vertex] == 3:
                 return False
 
-            color[vertex] = 2
+            c[vertex] = 2
 
             for n in graph[vertex]:
-                if color[n] == 2:
+                if c[n] == 2:
                     return True
                 
                 if cycle(n):
                     return True
 
-            color[vertex] = 3
+            c[vertex] = 3
             return False
         
         
