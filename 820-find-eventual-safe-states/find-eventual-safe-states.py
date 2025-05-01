cfc6d0c6681a12b6c24fd782fabdd98082c87e91
AlazarGebre <151013856+AlazarGebre21@users.noreply.github.com>
2025-05-01 23:52:41 +0300

    Time: 75 ms (22.32%) | Memory: 23.8 MB (41.15%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.py
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -3,30 +3,32 @@ class Solution:
         
         reverse_graph = defaultdict(list)
         n = len(graph)
-        outdegree = [0] * n
+        indegree = [0] * n
         
 
-        for i in range(n):  
-            for u in graph[i]:
-                reverse_graph[u].append(i)
-                outdegree[i] += 1
-        
-        que = deque([i for i in range(n) if outdegree[i] == 0])
-        safe_node = [False] * n
+        for parent in range(n):  
+            for child in graph[parent]:
+                reverse_graph[child].append(parent)
+                indegree[parent] += 1
+
+        print(indegree)
+         
+        que = deque([i for i in range(n) if indegree[i] == 0])
+        safest_node = [False] * n
 
         while que:
 
             terminal_node = que.popleft()
-            safe_node[terminal_node] = True
+            safest_node[terminal_node] = True
 
             for neighbour in reverse_graph[terminal_node]:
-                outdegree[neighbour] -= 1
+                indegree[neighbour] -= 1
 
-                if outdegree[neighbour] == 0:
+                if indegree[neighbour] == 0:
                     que.append(neighbour)
         
 
-        return [i for i in range(n) if safe_node[i]]
+        return [i for i in range(n) if safest_node[i]]
 
         
                      
