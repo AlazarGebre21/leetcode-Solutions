cf25f9b7b10bc1cd54c23f6931683bacc538399b
AlazarGebre <151013856+AlazarGebre21@users.noreply.github.com>
2025-05-02 17:10:25 +0300

    Time: 24 ms (10.21%) | Memory: 19.1 MB (41.92%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,35 +1,55 @@
+class UnionFind:
+    def __init__(self, n):
+
+        self.parent = {i: i for i in range(n)}
+        self.size = [1] * (n)
+    
+    def find(self,x):
+
+        if self.parent[x] == x:
+            return x
+
+        self.parent[x] = self.find(self.parent[x])
+
+        return self.parent[x]
+
+    def union(self, x, y):
+        
+        parent_x = self.find(x)
+        parent_y = self.find(y)
+
+        if self.size[parent_x] > self.size[parent_y]:
+            self.parent[parent_y] = parent_x
+            self.size[parent_x] += self.size[parent_y]
+
+        elif self.size[parent_y] > self.size[parent_x]:
+            self.parent[parent_x] = parent_y
+            self.size[parent_y] += self.size[parent_x]
+
+        else:
+            self.parent[parent_y]  = parent_x
+            self.size[parent_x] += self.size[parent_y]
+        
+
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        count_provinces = 0
-        rows = len(isConnected)
-        cols = len(isConnected[0])
-        graph = defaultdict(set)
-        visited = set()
         
-        for r in range(rows):
-            for c in range(cols):
+        n = len(isConnected)
+        uf = UnionFind(n)
+
+        for r in range(n):
+            for c in range(n):
                 if isConnected[r][c] == 1:
-                    graph[r].add(c)
-                    graph[c].add(r)
-
-        print(graph)
+                    uf.union(r, c)
         
-        def dfs(vertex):
+        no_provinces = 0
 
-           
-            visited.add(vertex)
-
-            for n in graph[vertex]:
-                if n not in visited:
-                    dfs(n)
-
-            return 1
+        for key, value in uf.parent.items():
+            if key == value:
+                no_provinces += 1
         
-
-        for k in graph:
-            if k not in visited:
-                count_provinces += dfs(k)
-        
-        return count_provinces
+        return no_provinces
 
             
+                
+        
