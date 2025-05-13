08b6f83ab0594e80fca0cfbbab3dbff9b8df3e37
AlazarGebre <151013856+AlazarGebre21@users.noreply.github.com>
2025-05-13 19:45:18 +0300

    Time: 3 ms (63.68%) | Memory: 18.7 MB (51.92%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -2,9 +2,15 @@ class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         
         n = len(nums)
-        nums.sort()
+        xor_idx = 0
+        xor_val = 0
 
-        for i in range(len(nums)):
-            if i != nums[i]:
-                return i
-        return n
+        for idx, val in enumerate(nums):
+
+            xor_idx = xor_idx ^ (idx + 1)
+
+            xor_val = xor_val ^ val
+
+        
+        return xor_idx ^ xor_val
+        
