5214fbac3a76416a236a32da366059b00333a931
AlazarGebre <151013856+AlazarGebre21@users.noreply.github.com>
2025-05-14 15:33:01 +0300

    Time: 3 ms (94.14%) | Memory: 18.6 MB (59.53%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -2,30 +2,20 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
 
 
-        current_base = 2
-        future_base = current_base * 2
-
         ans = [0] * (n+1)
-        ans[0] = 0
+        
 
-        for i in range(1, n+1):
+        for i in range(n+1):
 
-            if i >= 3:
-
-                if future_base == i:
-                    ans[i] = 1
-                    current_base = future_base
-                    future_base = current_base * 2
-                    
-                else:
-                    rem = i - current_base
-                    ans[i] = ans[current_base] + ans[rem]
-            
+            pre = i // 2
+            if i % 2 == 0:
+                ans[i] = ans[pre]
             else:
-                ans[i] = 1
+                ans[i] = ans[pre] + 1
         
         return ans
 
 
+            
 
 
