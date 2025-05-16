c0bd9710cde61c4e24e8b62a144b9a84fd3f53c5
AlazarGebre <151013856+AlazarGebre21@users.noreply.github.com>
2025-05-16 13:41:44 +0300

    Time: 278 ms (68.50%) | Memory: 20.8 MB (51.64%) - LeetSync

--- 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.py
+++ 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.py
@@ -1,31 +1,19 @@
 class Solution:
     def maxProduct(self, words: List[str]) -> int:
-
-
-        def is_equal(arr1, arr2,my_dict):
-            xor_sum = 0
-            normal_sum = 0
-
-            for char1 in arr1:
-                xor_sum = xor_sum ^ my_dict[char1]
-                normal_sum = normal_sum + my_dict[char1]
-            
-            for char2 in arr2:
-                xor_sum = xor_sum ^ my_dict[char2]
-                normal_sum = normal_sum + my_dict[char2]
-
-            return xor_sum == normal_sum
-
         
         max_len = 0
         my_dict = {chr(char): 1 << char - ord('a')  for char in range(ord('a'), ord('z') + 1)}
 
         # print(my_dict)
-        arr = [0] * len(words)
+        arr = []
+        for word in words:
+            _or = 0
 
-        for i in range(len(words)):
-            arr[i] = list(set(words[i]))
-
+            for i in word:
+                _or |= my_dict[i]
+            
+            arr.append(_or)
+       
         # print(arr)
 
         for i in range(len(arr)):
@@ -33,8 +21,10 @@ class Solution:
             for j in range(i + 1, len(arr)):
                 second_len = len(words[j])
 
-                if is_equal(arr[i],arr[j],my_dict):
+                if arr[i] & arr[j] == 0:
                     max_len = max(max_len, first_len * second_len)
+
+                
         
 
         return max_len
