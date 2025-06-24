d6df6e2f6b41b223e7acae46b961044442b71e82
AlazarGebre <151013856+AlazarGebre21@users.noreply.github.com>
2025-06-24 14:44:06 +0300

    Time: 3 ms (22.16%) | Memory: 17.8 MB (80.50%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,25 +1,26 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        my_dict = {'(':')','{':'}','[':']'}
+
+        my_dict = {'(':')', '[':']','{':'}'}
         stack = []
-        
-        for parenthesis in s:
-            if parenthesis in my_dict.keys():
-                stack.append(parenthesis)
-            elif parenthesis in my_dict.values():
-                if stack:
-                    closing = parenthesis
-                    opening = stack.pop()
-                    if my_dict[opening] != closing:
+        for bracket in s:
+            if bracket in my_dict.keys():
+                stack.append(bracket)      
+            else:
+                if len(stack) != 0:
+                    if my_dict[stack[-1]] == bracket:
+                        stack.pop()
+                    else:
                         return False
                 else:
                     return False
         
-        return stack == []
-                        
-                        
-                        
-                        
+        if len(stack) == 0:
+            return True
+        else:
+            return False
+
+    
 
         
         
