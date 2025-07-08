4c79e24061ae6d46d42b3425bed16418840e7434
AlazarGebre <151013856+AlazarGebre21@users.noreply.github.com>
2025-07-08 13:14:47 +0300

    Time: 0 ms (100.00%) | Memory: 18.4 MB (93.11%) - LeetSync

--- 859-design-circular-deque/design-circular-deque.py
+++ 859-design-circular-deque/design-circular-deque.py
@@ -2,68 +2,67 @@ class MyCircularDeque:
 
     def __init__(self, k: int):
         self.k = k
-        self.my_circular_que = deque([])
+        self.cir_que = deque([])
 
     def insertFront(self, value: int) -> bool:
-        if len(self.my_circular_que) < self.k:
-            self.my_circular_que.appendleft(value)
+        if len(self.cir_que) < self.k:
+            self.cir_que.appendleft(value)
             return True
         else:
             return False
-        
-
     def insertLast(self, value: int) -> bool:
-        if len(self.my_circular_que) < self.k:
-            self.my_circular_que.append(value)
+        if len(self.cir_que) < self.k:
+            self.cir_que.append(value)
             return True
         else:
             return False
         
-
     def deleteFront(self) -> bool:
-        if len(self.my_circular_que) == 0:
-            return False
-        else:
-            self.my_circular_que.popleft()
+        if len(self.cir_que) != 0:
+            self.cir_que.popleft()
             return True
+        else:
+            return False
         
 
     def deleteLast(self) -> bool:
-        if len(self.my_circular_que) == 0:
-            return False
-        else:
-            self.my_circular_que.pop()
+        if len(self.cir_que) != 0:
+            self.cir_que.pop()
             return True
+        else:
+            return False
         
-
     def getFront(self) -> int:
-        if len(self.my_circular_que) == 0:
-            return -1
+        if len(self.cir_que) != 0:
+            return self.cir_que[0]
+            
         else:
-            return self.my_circular_que[0]
-
+            return -1
+        
     def getRear(self) -> int:
-        if len(self.my_circular_que) == 0:
-            return -1
+        if len(self.cir_que) != 0:
+            return self.cir_que[-1]
+            
         else:
-            return self.my_circular_que[-1]
+            return -1
         
     def isEmpty(self) -> bool:
-        if len(self.my_circular_que) == 0:
-            return True
-        else:
+        if len(self.cir_que) != 0:
             return False
             
-    def isFull(self) -> bool:
-        if len(self.my_circular_que) == self.k:
-            return True
         else:
+            return True
+
+    def isFull(self) -> bool:
+        if len(self.cir_que) != self.k:
             return False
+        else:
+            return True
         
 
 
 # Your MyCircularDeque object will be instantiated and called as such:
-# obj = MyCircularDeque(k)
+# obj = MyCircularDeque(self.k)
 # param_1 = obj.insertFront(value)
 # param_2 = obj.insertLast(value)
 # param_3 = obj.deleteFront()
