6668f42caa08906c9744eb71083224047dc97a36
AlazarGebre <151013856+AlazarGebre21@users.noreply.github.com>
2025-08-29 00:31:04 +0300

    Time: 197 ms (46.42%) | Memory: 22.1 MB (54.13%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -1,54 +1,27 @@
+from typing import List
+
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
-        
-        def checker(mid):
-            sums = 0
-            counts = 1
-            i = 0
-            
-            while i < len(weights):
 
-                sums += weights[i]
+        def can_ship(capacity: int) -> bool:
+            ships = 1  
+            current_load = 0
 
-                if sums > mid:
-                    sums = weights[i]
-                    counts += 1
+            for w in weights:
+                if current_load + w > capacity:
+                    ships += 1
+                    current_load = 0
+                current_load += w
 
-                    if counts > days:
-                        return False
-                
-                i += 1
-            
-            
-            return True
+            return ships <= days
 
-        
+        low, high = max(weights), sum(weights)
 
-        low = max(weights)
-        high = sum(weights)
-
-        
-
-        while low <= high:
-            
+        while low < high:
             mid = (low + high) // 2
-         
-            if checker(mid):
-                high = mid - 1
-
+            if can_ship(mid):
+                high = mid 
             else:
-                low = mid + 1
-            
+                low = mid + 1 
+
         return low
-        
-
-            
-
-                
-        
-
-
-            
-
-
-                
