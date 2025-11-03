e9f9d276dd22095eac8bd17137cbfd14cb3ff419
Alazar Gebre <151013856+AlazarGebre21@users.noreply.github.com>
2025-11-04 01:24:21 +0300

    Time: 0 ms (100.00%) | Memory: 57.2 MB (11.78%) - LeetSync

---
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} hours
+ * @param {number} target
+ * @return {number}
+ */
+var numberOfEmployeesWhoMetTarget = function(hours, target) {
+
+    let count = 0
+
+    for(let hour of hours){
+        if (hour >= target) {
+            count += 1
+        }
+    }
+    
+    return count
+};
