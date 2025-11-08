90fc4935a40435513ea1909d8cd10d2eea84f631
Alazar Gebre <151013856+AlazarGebre21@users.noreply.github.com>
2025-11-09 01:36:38 +0300

    Time: 35 ms (95.00%) | Memory: 54 MB (58.42%) - LeetSync

---
+++ 2732-counter/counter.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+let temp = 0
+var createCounter = function(n) {
+    let count = n
+
+
+    
+    return function() {
+        return count++
+
+        
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
